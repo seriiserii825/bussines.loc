e09c9132b668d08b6a8b1e67f36f8466afa1a493
serii <seriiburduja@gmail.com>
2019-05-31 19:21:48 +0300

    + styles

--- wp-content/themes/bussines.loc/functions.php
+++ wp-content/themes/bussines.loc/functions.php
@@ -7,39 +7,16 @@
  * @package bussines
  */
 
+require_once __DIR__.'/inc/carbon-fields/carbon-fields.php';
+
 if ( ! function_exists( 'bussines_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
 	function bussines_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on bussines, use a find and replace
-		 * to change 'bussines' to the name of your theme in all the template files.
-		 */
 		load_theme_textdomain( 'bussines', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
@@ -47,10 +24,6 @@ if ( ! function_exists( 'bussines_setup' ) ) :
 			'menu-1' => esc_html__( 'Primary', 'bussines' ),
 		) );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support( 'html5', array(
 			'search-form',
 			'comment-form',
@@ -59,7 +32,6 @@ if ( ! function_exists( 'bussines_setup' ) ) :
 			'caption',
 		) );
 
-		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'bussines_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
@@ -68,11 +40,6 @@ if ( ! function_exists( 'bussines_setup' ) ) :
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support( 'custom-logo', array(
 			'height'      => 250,
 			'width'       => 250,
@@ -83,13 +50,6 @@ if ( ! function_exists( 'bussines_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'bussines_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
 function bussines_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
@@ -98,11 +58,6 @@ function bussines_content_width() {
 }
 add_action( 'after_setup_theme', 'bussines_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function bussines_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'bussines' ),
@@ -116,15 +71,20 @@ function bussines_widgets_init() {
 }
 add_action( 'widgets_init', 'bussines_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
 function bussines_scripts() {
-	wp_enqueue_style( 'bussines-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'business-style', get_stylesheet_uri() );
+	wp_enqueue_style('business-grid-css', get_template_directory_uri().'/assets/css/grid.css');
+	wp_enqueue_style('business-style-css', get_template_directory_uri().'/assets/css/style.css');
+	wp_enqueue_style('business-colorScheme-4-css', get_template_directory_uri().'/assets/css/colorScheme-4.css');
+	wp_enqueue_style('business-camera-css', get_template_directory_uri().'/assets/css/camera.css');
+	wp_enqueue_style('business-owl-carousel-css', get_template_directory_uri().'/assets/css/owl-carousel.css');
 
-	wp_enqueue_script( 'bussines-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bussines-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', false, null, true );
+	wp_enqueue_script( 'jquery' );
+
+	wp_enqueue_script( 'business-script-js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -132,29 +92,6 @@ function bussines_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'bussines_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
