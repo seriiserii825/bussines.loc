7f78e6ea29027967312f4693043012b03a648fec
serii <seriiburduja@gmail.com>
2019-06-03 18:26:56 +0300

    + soc icons

--- wp-content/themes/bussines.loc/footer.php
+++ wp-content/themes/bussines.loc/footer.php
@@ -1,23 +1,39 @@
 <footer>
-    <section class="well3">
-        <div class="container">
-            <ul class="row contact-list">
-				<li class="grid_4">
-					<div class="box">
-						<div class="box_aside">
-							<div class="icon2 "></div>
-						</div>
-						<div class="box_cnt__no-flow">
-							   <address></address>
-								<a href="callto:<?php  ?>"><?php  ?></a>
-								<a href="mailto:<?php  ?>"><?php  ?></a>
-								<a href="<?php  ?></a>
-						</div>
-					</div>
-				</li>
-            </ul>
-        </div>
-    </section>
+    <?php $socials_icons = carbon_get_the_post_meta('crb_social_icons'); ?>
+
+    <?php if($socials_icons): ?>
+		<section class="well3">
+			<div class="container">
+				<ul class="row contact-list">
+					<?php $i = 1; foreach ($socials_icons as $soc_icon): ?>
+                        <?php if($i % 2 != 0): ?>
+							<li class="grid_4">
+							<?php endif; ?>
+								<div class="box">
+									<div class="box_aside">
+										<div class="icon2 <?php echo $soc_icon['icon'] ?>"></div>
+									</div>
+									<div class="box_cnt__no-flow">
+                                        <?php $soc_icon_sel = $soc_icon['crb_social_icons_select']; ?>
+                                        <?php if($soc_icon_sel === 'address'): ?>
+											<address><?php echo $soc_icon['link']; ?></address>
+                                        <?php elseif ($soc_icon_sel === 'email'): ?>
+											<a href="mailto:<?php echo $soc_icon['link']; ?>"><?php echo $soc_icon['link']; ?></a>
+                                        <?php elseif ($soc_icon_sel === 'phone'): ?>
+                                            <a href="callto:<?php echo str_replace(['-', '(', ')'], '', $soc_icon['phone']); ?>"><?php echo $soc_icon['link']; ?></a>
+                                        <?php else:  ?>
+                                            <a href="#"><?php echo $soc_icon['link']; ?></a>
+                                        <?php endif; ?>
+                                    </div>
+								</div>
+							<?php if($i % 4 == 0): ?>
+							</li>
+							<?php endif; ?>
+					<?php $i++; endforeach; ?>
+				</ul>
+			</div>
+		</section>
+    <?php endif; ?>
     <section>
         <div class="container">
             <div class="copyright">Business Company © <span id="copyright-year"></span>.&nbsp;&nbsp;<a href="index-5.html">Privacy Policy</a>More Business Website Templates at <a rel="nofollow" href="http://www.templatemonster.com/category/business-web-templates/" target="_blank">TemplateMonster.com</a>
